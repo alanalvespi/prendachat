d3e6c04c721a2f63c7466282fa75874f4c6fe383
Alan Alvarado <alan.alvespi@gmail.com>
2015-07-16 08:07:48 -0500

    Update port and server ip

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var express = require('express')
 , _ = require('underscore')._;
 
 app.configure(function() {
-	app.set('port', process.env.PORT || 8080);
-  	app.set('ipaddr', process.env.IP || "127.0.0.1");
+	app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
+  	app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(express.static(__dirname + '/public'));
@@ -33,12 +33,13 @@ app.get('/', function(req, res) {
   res.render('index.html');
 });
 
+app.get('/', function(req, res) {
+  res.render('index.html');
+});
+
 server.listen(app.get('port'), app.get('ipaddr'), function(){
 	console.log('Express server listening on  IP: ' + app.get('ipaddr') + ' and port ' + app.get('port'));
 });
-app.listen(app.get('port'), function() {
-  console.log("Node app is running on port:" + app.get('port'))
-})
 
 io.set("log level", 1);
 var people = {};
